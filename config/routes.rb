4e1e4341f5859d61c9fe6c44feedf0664c7d62d1
Tharit Pongsaneh <tharit.p@ku.th>
2017-05-08 08:54:36 +0000

    First Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
-Rails.application.routes.draw do 
-  get 'static_pages/home'
-  get 'static_pages/help'
-root 'application#hello'
+Rails.application.routes.draw do
+root 'static_pages#home'
+get '/help', to: 'static_pages#help'
+get '/about', to: 'static_pages#about' 
+get '/contact', to: 'static_pages#contact'
+get '/home', to: 'static_pages#home'
+get '/login', to: 'static_pages#login'
+
 end
